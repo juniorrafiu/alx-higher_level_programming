3323538ac342a988a6816009ea0bffa9c2f87353
Abdulai <juniorrafiu@gmail.com>
2023-06-08 17:43:05 -0700

    modules and packages

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -9,6 +9,6 @@ if __name__ == "__main__":
             print("1 argument.")
             print("{} : {}".format(1, sys.argv[1]))
         elif (len(sys.argv)-1 > 1):
-            print("{} arguments:".format(len(sys.argv)-1 ))
-            for i in range(1,len(sys.argv)):
+            print("{} arguments:".format(len(sys.argv)-1))
+            for i in range(1, len(sys.argv)):
                 print("{} : {}".format(i, sys.argv[i]))
--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -2,6 +2,6 @@
 if __name__ == "__main__":
     import sys
     result = 0
-    for i in range(1,len(sys.argv)):
+    for i in range(1, len(sys.argv)):
         result += int(sys.argv[i])
     print(result)
