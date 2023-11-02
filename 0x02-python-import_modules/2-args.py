d54d828d6aec1ae917a103099b3e6d9c0f42208d
juniorrafiu <juniorrafiu7@gmail.com>
2023-11-02 08:05:45 -0700

    hard

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -3,8 +3,8 @@ if __name__ == "__main__":
     from sys import argv
     argNum = len(argv) - 1
     if argNum == 1:
-        print('{} argument:\n'.format(argNum))
+        print('{} argument:'.format(argNum))
     else:
-        print('{} arguments:\n'.format(argNum))
-    for i in range(argNum):
-        print('{}: {}:\n'.format(i, argv[i]))
+        print('{} arguments:'.format(argNum))
+    for i in range(1, argNum + 1):
+        print('{}: {}'.format(i, argv[i]))
