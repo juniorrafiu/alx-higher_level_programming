049ec63c0dc1d2b8002b69a5d5fb5b259c330a3e
juniorrafiu <juniorrafiu7@gmail.com>
2023-11-02 08:42:13 -0700

    hard

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -4,5 +4,5 @@ if __name__ == "__main__":
     argNum = len(argv) - 1
     sum = 0
     for i in range(1, argNum + 1):
-        sum = sum + int(argv[i]) 
+        sum = sum + int(argv[i])
     print(sum)
