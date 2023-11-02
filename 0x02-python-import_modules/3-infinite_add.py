fbedd6af3d672053ba49420437bdc7edf8fa60c2
juniorrafiu <juniorrafiu7@gmail.com>
2023-11-02 08:40:09 -0700

    hard

---
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    from sys import argv
+    argNum = len(argv) - 1
+    sum = 0
+    for i in range(1, argNum + 1):
+        sum = sum + int(argv[i]) 
+    print(sum)
