b03249be4d1bcbbb1481288f28c25dc73f09a0eb
juniorrafiu <juniorrafiu7@gmail.com>
2023-11-21 16:47:31 -0800

    hard

---
+++ 0x06-python-classes/5-square.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+class Square:
+    def __init__(self, size=0):
+        self.__size = size
+
+    def area(self):
+        return (self.__size ** 2)
+
+    @size.setter
+    def size(self, value):
+        if type(size) != int:
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
+
+    @property
+    def size(self):
+        return self.__size
+
+    def my_print(self):
+        for i in range(0, self.__size):
+            [print("#", end="") for j in range(self.__size)]
+            print("")
+        if self.__size == 0:
+            print("")
