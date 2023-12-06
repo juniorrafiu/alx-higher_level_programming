332c22909b9a10ed1df97472bcd092a47d0b6259
juniorrafiu <juniorrafiu7@gmail.com>
2023-12-06 06:48:27 -0800

    hard

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,17 +1,16 @@
 #!/usr/bin/python3
-"""
-this module contains the add_item function
-"""
-
-
+"""Adds arguments to a python list and appends them to a file."""
 import sys
-from os import path
 
 
-save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
-load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
+if __name__ == "__main__":
+    save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
+    load_from_json_file = __import__(
+            '6-load_from_json_file').load_from_json_file
 
-new_list = []
-if path.isfile("add_item.json"):
-    new_list = load_from_json_file("add_item.json")
-save_to_json_file(new_list + sys.argv[1:], "add_item.json")
+    try:
+        items = load_from_json_file("add_item.json")
+    except FileNotFoundError:
+        items = []
+    items.extend(sys.argv[1:])
+    save_to_json_file(items, "add_item.json")
