3721f5dd9f477ad99241245f9d77d0130e33a703
juniorrafiu <juniorrafiu7@gmail.com>
2024-01-18 09:30:06 -0800

    hard

---
+++ 0x0D-SQL_introduction/10-top_score.sql
@@ -0,0 +1,3 @@
+-- displays both score and name columns in descending order of score
+SELECT score, name FROM second_table
+ORDERBY score DESC;
---
+++ 0x0D-SQL_introduction/2-remove_database.sql
@@ -0,0 +1,2 @@
+-- deletes a database and does not fail even if there's no databas
+DROP DATABASE IF EXISTS hbtn_0c_0;
