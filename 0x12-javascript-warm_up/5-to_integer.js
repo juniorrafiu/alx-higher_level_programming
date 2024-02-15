dc8546b74ef089a33d81fc71d1f2a5c5d3acaba2
juniorrafiu <juniorrafiu7@gmail.com>
2024-02-14 17:39:31 -0800

    hard

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 const x = Number(process.argv[2]);
 if (isNaN(x)) {
-    console.log('Not a number');
-  } else {
-    console.log(`My number: ${x}`);
-  }
+  console.log('Not a number');
+} else {
+  console.log(`My number: ${x}`);
+}
